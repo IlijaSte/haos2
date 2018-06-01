23f17dd73c9094a441b0f1dcac6045213e8bb1e5
IlijaSte <ilijaste96@gmail.com>
2018-06-01 23:43:03 +0200

    initial commit

---
+++ Haos2/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour {
+
+    private float initX;
+
+    private void Update()
+    {
+        Vector2 localPoint = Vector2.zero;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(this.GetComponent<RectTransform>(), Input.mousePosition, Camera.main, localPoint))
+        {
+
+            if (localPoint.x < 0)
+                print("Left!");
+            else print("Right!");
+        }
+    }
+}
