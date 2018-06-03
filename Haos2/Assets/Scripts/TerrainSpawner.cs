273c1548c6536aeebc540cb46af98b1d304e0197
IlijaSte <ilijaste96@gmail.com>
2018-06-03 22:20:20 +0200

    Poboljsano kretanje loptice

---
+++ Haos2/Assets/Scripts/TerrainSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainSpawner : MonoBehaviour {
+
+    public Transform ball;
+    [SerializeField]
+    public GameObject[] terrainPrefabs;
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Vector3.Distance(ball.position, BallBehaviour.currTerrain.position) > BallBehaviour.currTerrain.GetComponent<TerrainInfo>().length / 2 && !BallBehaviour.spawnedNext)
+        {
+            GameObject newObj = Instantiate(terrainPrefabs[Random.Range(0, terrainPrefabs.Length)]);
+            newObj.transform.position = BallBehaviour.currTerrain.position + new Vector3(BallBehaviour.currTerrain.GetComponent<TerrainInfo>().length, 0, 0);
+            BallBehaviour.spawnedNext = true;
+        }
+
+    }
+}
